f41bcaf5fa1740da42c8a6a4d82cffc9022ce76f
MacyE-Baker <macybaker1111@gmail.com>
2026-03-05 11:25:12 -0600

    Updated order

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,83 @@
--- Macy's code!!
 CREATE DATABASE airline_ticketing;
 USE airline_ticketing;
+
+-- Country Object
+CREATE TABLE Country (
+    country_id   INTEGER PRIMARY KEY,
+    country_name   VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE city (
+    city_id INTEGER PRIMARY KEY,
+    city_name VARCHAR(60)
+);
+
+CREATE TABLE airline (
+    airline_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    airline_name VARCHAR(60) NOT NULL,
+    airline_code VARCHAR(10) NOT NULL UNIQUE
+);
+
+CREATE TABLE Airport (
+    airport_id   INTEGER PRIMARY KEY NOT NULL,
+    airport_code   VARCHAR(10),
+    airport_name   VARCHAR(60),
+    country_id   INTEGER NOT NULL,
+    city_id   INTEGER NOT NULL,
+    timezone   VARCHAR(30),
+    number_of_terminals   INTEGER,
+    FOREIGN KEY (country_id) REFERENCES Country(country_id),
+    FOREIGN KEY (city_id)    REFERENCES City(city_id)
+);
+
+CREATE TABLE gates(
+	gate_id INTEGER PRIMARY KEY,
+	airport_id INTEGER, 
+	gate_name VARCHAR(10),
+	FOREIGN KEY (airport_id) 
+		REFERENCES airport (airport_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+-- Aircraft:
+CREATE TABLE Aircraft (
+    aircraft_id INT PRIMARY KEY,
+    model VARCHAR(30) NOT NULL,
+    manufacturer VARCHAR(30) NOT NULL,
+    seating_capacity INT NOT NULL CHECK (seating_capacity > 0),
+    max_baggage_capacity INT CHECK (max_baggage_capacity >= 0)
+);
+
+CREATE TABLE routes(
+	route_id INTEGER PRIMARY KEY, 
+	departure_airport_id INTEGER,
+	destination_airport_id INTEGER,
+	demand ENUM ('Low', 'Medium', 'High'),
+	FOREIGN KEY (departure_airport_id) 
+		REFERENCES airport (airport_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE,
+	FOREIGN KEY (destination_airport_id) 
+		REFERENCES airport (airport_id)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+);
+
+CREATE TABLE Flights (
+    flight_id INT PRIMARY KEY,
+    aircraft_id INT,
+    route_id INT,
+    airline_id INT,
+    date_of_departure DATETIME NOT NULL,
+    total_seats INT NOT NULL CHECK (total_seats > 0),
+    seats_available INT NOT NULL CHECK (seats_available >= 0),
+    staff_size INT NOT NULL CHECK (staff_size >= 4),
+    baggage_capacity INT CHECK (baggage_capacity >= 0),
+    baggage_availability INT CHECK (baggage_availability >= 0),
+
+    CHECK (seats_available <= total_seats)
+);
 
 -- passenger object
 CREATE TABLE Passenger (
@@ -19,19 +96,6 @@ CREATE TABLE Passenger (
     meal_preferences   VARCHAR(60),
     special_needs      VARCHAR(120),
     FOREIGN KEY (country_of_origin) REFERENCES Country(country_id)
-);
-
--- Airport Object
-CREATE TABLE Airport (
-    airport_id   INTEGER PRIMARY KEY NOT NULL,
-    airport_code   VARCHAR(10),
-    airport_name   VARCHAR(60),
-    country_id   INTEGER NOT NULL,
-    city_id   INTEGER NOT NULL,
-    timezone   VARCHAR(30),
-    number_of_terminals   INTEGER,
-    FOREIGN KEY (country_id) REFERENCES Country(country_id),
-    FOREIGN KEY (city_id)    REFERENCES City(city_id)
 );
 
 -- Employee Object
@@ -62,31 +126,26 @@ CREATE TABLE Employee (
     FOREIGN KEY (airport_id)  REFERENCES Airport(airport_id)
 );
 
--- Country Object
-CREATE TABLE Country (
-    country_id   INTEGER PRIMARY KEY,
-    country_name   VARCHAR(60) NOT NULL
+CREATE TABLE user_account (
+    user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    passenger_id INTEGER NOT NULL UNIQUE,
+    email VARCHAR(120) NOT NULL UNIQUE,
+    password VARCHAR(30) NOT NULL,
+    card_number VARCHAR(30),
+    card_expiration_date DATE,
+    card_security_code VARCHAR(10),
+    role ENUM('Passenger', 'Employee') NOT NULL,
+    FOREIGN KEY (passenger_id) REFERENCES Passenger(passenger_id)
 );
 
-
-
-
--- Eman's Code!
-CREATE TABLE Flights (
-    flight_id INT PRIMARY KEY,
-    aircraft_id INT,
-    route_id INT,
-    airline_id INT,
-    date_of_departure DATETIME NOT NULL,
-    total_seats INT NOT NULL CHECK (total_seats > 0),
-    seats_available INT NOT NULL CHECK (seats_available >= 0),
-    staff_size INT NOT NULL CHECK (staff_size >= 4),
-    baggage_capacity INT CHECK (baggage_capacity >= 0),
-    baggage_availability INT CHECK (baggage_availability >= 0),
-
-    CHECK (seats_available <= total_seats)
+CREATE TABLE loyalty_program (
+    loyalty_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    passenger_id INTEGER NOT NULL UNIQUE,
+    membership_number VARCHAR(30) NOT NULL,
+    tier ENUM('Silver', 'Gold', 'Platinum'),
+    miles_balance INTEGER DEFAULT 0,
+    FOREIGN KEY (passenger_id) REFERENCES Passenger(passenger_id)
 );
-
 
 CREATE TABLE Bookings (
     booking_id INT PRIMARY KEY,
@@ -102,7 +161,6 @@ CREATE TABLE Bookings (
 	-- end of new code
     FOREIGN KEY (flight_id) REFERENCES Flights(flight_id)
 );
-
 
 CREATE TABLE Payment (
     payment_id INT PRIMARY KEY,
@@ -121,47 +179,6 @@ CREATE TABLE Payment (
     FOREIGN KEY (booking_id) REFERENCES Bookings(booking_id)
 );
 
-
-CREATE TABLE Booking_Passengers (
-    passenger_id INT NOT NULL,
-    booking_id INT NOT NULL,
-
-    PRIMARY KEY (passenger_id, booking_id),
-
-    FOREIGN KEY (passenger_id) REFERENCES Passenger(passenger_id),
-    FOREIGN KEY (booking_id) REFERENCES Bookings(booking_id)
-);
-
-
-
-
-
--- Jashan’s code
--- Aircraft:
-CREATE TABLE Aircraft (
-    aircraft_id INT PRIMARY KEY,
-    model VARCHAR(30) NOT NULL,
-    manufacturer VARCHAR(30) NOT NULL,
-    seating_capacity INT NOT NULL CHECK (seating_capacity > 0),
-    max_baggage_capacity INT CHECK (max_baggage_capacity >= 0)
-);
-
-
--- Insurance policy:
-CREATE TABLE Insurance_Policy (
-    insurance_id INT PRIMARY KEY,
-    booking_id INT NOT NULL,
-    provider_name ENUM('Allianz','Global Travel','Safe Trip','Other') NOT NULL,
-    coverage_amount DECIMAL(7,2) NOT NULL CHECK (coverage_amount >= 0),
-    insurance_fee DECIMAL(5,2) NOT NULL CHECK (insurance_fee >= 0),
-    coverage_type ENUM('Trip Cancellation','Medical','Baggage','Full Coverage') NOT NULL,
-    FOREIGN KEY (booking_id) REFERENCES Bookings(booking_id)
-);
-
-
-
-
--- Alan's Code
 CREATE TABLE boardingpass(
 	boarding_pass_id INTEGER PRIMARY KEY,
 	airport_id INTEGER,
@@ -191,65 +208,24 @@ CREATE TABLE boardingpass(
 		ON UPDATE CASCADE
 );
 
-CREATE TABLE routes(
-	route_id INTEGER PRIMARY KEY, 
-	departure_airport_id INTEGER,
-	destination_airport_id INTEGER,
-	demand ENUM ('Low', 'Medium', 'High'),
-	FOREIGN KEY (departure_airport_id) 
-		REFERENCES airport (airport_id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE,
-	FOREIGN KEY (destination_airport_id) 
-		REFERENCES airport (airport_id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
+CREATE TABLE Insurance_Policy (
+    insurance_id INT PRIMARY KEY,
+    booking_id INT NOT NULL,
+    provider_name ENUM('Allianz','Global Travel','Safe Trip','Other') NOT NULL,
+    coverage_amount DECIMAL(7,2) NOT NULL CHECK (coverage_amount >= 0),
+    insurance_fee DECIMAL(5,2) NOT NULL CHECK (insurance_fee >= 0),
+    coverage_type ENUM('Trip Cancellation','Medical','Baggage','Full Coverage') NOT NULL,
+    FOREIGN KEY (booking_id) REFERENCES Bookings(booking_id)
 );
 
-CREATE TABLE gates(
-	gate_id INTEGER PRIMARY KEY,
-	airport_id INTEGER, 
-	gate_name VARCHAR(10),
-	FOREIGN KEY (airport_id) 
-		REFERENCES airport (airport_id)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-);
+CREATE TABLE Booking_Passengers (
+    passenger_id INT NOT NULL,
+    booking_id INT NOT NULL,
 
-CREATE TABLE city(
-	city_id INTEGER PRIMARY KEY, 
-	city_name VARCHAR(60)
-);
+    PRIMARY KEY (passenger_id, booking_id),
 
-
-
-
--- Anitra's Code
-CREATE TABLE airline (
-    airline_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    airline_name VARCHAR(60) NOT NULL,
-    airline_code VARCHAR(10) NOT NULL UNIQUE
-);
-
-CREATE TABLE loyalty_program (
-    loyalty_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    passenger_id INTEGER NOT NULL UNIQUE,
-    membership_number VARCHAR(30) NOT NULL,
-    tier ENUM('Silver', 'Gold', 'Platinum'),
-    miles_balance INTEGER DEFAULT 0,
-    FOREIGN KEY (passenger_id) REFERENCES Passenger(passenger_id)
-);
-
-CREATE TABLE user_account (
-    user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    passenger_id INTEGER NOT NULL UNIQUE,
-    email VARCHAR(120) NOT NULL UNIQUE,
-    password VARCHAR(30) NOT NULL,
-    card_number VARCHAR(30),
-    card_expiration_date DATE,
-    card_security_code VARCHAR(10),
-    role ENUM('Passenger', 'Employee') NOT NULL,
-    FOREIGN KEY (passenger_id) REFERENCES Passenger(passenger_id)
+    FOREIGN KEY (passenger_id) REFERENCES Passenger(passenger_id),
+    FOREIGN KEY (booking_id) REFERENCES Bookings(booking_id)
 );
 
 CREATE TABLE inflight_entertainment (
@@ -261,7 +237,3 @@ CREATE TABLE inflight_entertainment (
     title_of_type VARCHAR(60) NOT NULL,
     description_of_type VARCHAR(120)
 );
-
-
-
-
