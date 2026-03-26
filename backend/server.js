fabe361e2588591dc662e683838aeee09c339bde
ag09mex <agger.agr.09@gmail.com>
2026-03-25 19:35:25 -0500

    changed database connection to hosted database

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const http = require("http");
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Houston2026!.",
-  database: "airline_system",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
